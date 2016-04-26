fe4f260432371f7cbaf07fed60c1cec715309066
Dan Robinson <danrobinson010@gmail.com>
2016-04-26 15:33:08 -0400

    added basic bootstrap templates

--- sample_app/flow/views.py
+++ sample_app/flow/views.py
@@ -1,8 +1,12 @@
 from django.http import HttpResponse
 from django.views.generic import View
+from django.shortcuts import render
 
 class StepView(View):
 
     def get(self, request, step_number):
         step_int = int(step_number)
-        return HttpResponse("Viewing step " + step_number)
+        context = {
+            "step_number": step_int
+        }
+        return render(request, 'flow/step.html', context)
