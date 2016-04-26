c278e20e8dba774bf49da04f54de8fc24742b246
Dan Robinson <danrobinson010@gmail.com>
2016-04-26 19:49:14 -0400

    created basic models for step and completedstep

--- sample_app/flow/models.py
+++ sample_app/flow/models.py
@@ -1,5 +1,13 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
+
+class Step(models.Model):
+    step_number = models.IntegerField(unique=True, db_index=True)
+
+class CompletedStep(models.Model):
+    user = models.ForeignKey(User)
+    step = models.ForeignKey(Step)
 
 # Create your models here.
